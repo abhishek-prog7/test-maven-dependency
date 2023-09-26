937a041dbadfb7bd2d64170e14ecbdfaa9931fd8
abhishek <abhishekchauhan10x@gmail.com>
2023-09-25 22:36:44 -0400

    Initial changes

---
+++ src/main/java/com/learning/jenkins/Main.java
@@ -0,0 +1,10 @@
+package com.learning.jenkins;
+
+public class messageSource {
+    
+    private String message = "Hello from the source message class";
+
+    public String getMessage(){
+        return message;
+    }
+}

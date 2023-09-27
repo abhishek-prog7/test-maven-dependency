e2f74043e1554076c6ccdee6d2be1a825c86b204
abhishek-prog7 <72608278+abhishek-prog7@users.noreply.github.com>
2023-09-27 07:48:54 +0530

    Update messageSource.java

--- src/main/java/com/learning/jenkins/messageSource.java
+++ src/main/java/com/learning/jenkins/messageSource.java
@@ -2,7 +2,7 @@ package com.learning.jenkins;
 
 public class messageSource {
     
-    private String message = "Hello from the source message class";
+    private String message = "Hello from the source message class, testing jenkins";
 
     public String getMessage(){
         return message;
